1db83f86abcb73d6caf5443a98a81cae4624bc67
jaqi <jaq.cat@protonmail.ch>
2021-09-17 21:10:56 -0300

    pogyears

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,37 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+type DbSchema = {
+  name: string;
+  x: number;
+  y: number;
+}
+
+let db: DbSchema[] = [];
 
 app.get('/', (req, res) => {
-  res.send("boop");
+  let idx: number = req.body.idx;
+  res.send({
+    suc: true,
+    data: db[idx]
+  });
+})
+
+app.post('/postsomething', (req, res) => {
+  console.log(req.body);
+  let idx = db.push({
+    name: req.body.name,
+    x: req.body.x,
+    y: req.body.y,
+  }) - 1;
+
+  res.send({
+    suc: true,
+    'id': idx,
+  });
 })
 
 const PORT = process.env.PORT || 5000;
