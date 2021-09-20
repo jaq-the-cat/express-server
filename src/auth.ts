b9488c88ddba41e73fcf8930e4859afe76bd81bd
Jaquie <jaq.cat@protonmail.ch>
2021-09-20 14:45:37 -0300

    stuff

--- src/auth.ts
+++ src/auth.ts
@@ -4,32 +4,41 @@ import db from './db';
 module.exports = (app: Express) => {
   app.get('/get', (req, res) => {
     let idx: number = req.body.idx;
-    res.send({
-      suc: true,
-      data: db[idx]
-    });
-  })
+    if (db.length <= idx)
+      res.send({
+        suc: false,
+      });
+    else
+      res.send({
+        suc: true,
+        data: db[idx]
+      });
+  });
 
   app.get('/all', (req, res) => {
     res.send({
       suc: true,
-      users: db.splice(0, req.body.limit),
+      users: db.slice(0, req.body.limit ?? 16),
     })
   });
 
   app.post('/signin', (req, res) => {
     const name = req.body.name;
-    const idx = db.findIndex((v) => v.name === name);
-    console.log(idx);
-    if (idx) {
+    const password = req.body.password;
+    const idx = db.findIndex((v) => v.name === name && v.password == password);
+    if (idx > -1) {
       req.session.signedin = true;
       req.session.name = name;
+      res.send({
+        suc: true,
+        id: idx,
+      });
+    } else {
+      res.send({
+        suc: false
+      });
     }
-    res.send({
-      suc: true,
-      id: idx,
-    });
-  })
+  });
 
   app.post('/signup', (req, res) => {
     let idx = db.push({
@@ -41,10 +50,10 @@ module.exports = (app: Express) => {
       suc: true,
       id: idx,
     });
-  })
+  });
 
   app.get('/signout', (req, res) => {
-    if (!req.session.signedin) {
+    if (req.session.signedin) {
       req.session.signedin = false;
       req.session.name = null;
       res.send({
@@ -55,7 +64,7 @@ module.exports = (app: Express) => {
         suc: false
       });
     }
-  })
+  });
 
   app.get('/issignedin', (req, res) => {
     res.send({
